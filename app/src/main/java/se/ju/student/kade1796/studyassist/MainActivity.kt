01898d8c9e886be91fe47eeacf5ceaafd6646822
Eddiemix33 <62739265+Eddiemix33@users.noreply.github.com>
2021-02-18 15:31:06 +0100

    Skapade en layout till main.
    Skapade en protoyp layout till main utan att lägga till någon funktionalitit ännu.
    Får se om man lägga till kategori knapparna på ett snyggt sätt.

--- app/src/main/java/se/ju/student/kade1796/studyassist/MainActivity.kt
+++ app/src/main/java/se/ju/student/kade1796/studyassist/MainActivity.kt
@@ -2,12 +2,19 @@ package se.ju.student.kade1796.studyassist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
-
-//This is Eddies project
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val category1_button = findViewById<ImageButton>(R.id.category1)
+
+        category1_button.setOnClickListener {
+            //Start next activity
+        }
     }
+
+
 }
