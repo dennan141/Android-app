98d943e4baead83f45cec3d33c459243857da0e5
Hawkar Jamal <jaha1710@student.ju.se>
2021-02-22 15:54:28 +0100

    done with create thread activity and added threads activity

--- app/src/main/java/se/ju/student/kade1796/studyassist/MainActivity.kt
+++ app/src/main/java/se/ju/student/kade1796/studyassist/MainActivity.kt
@@ -1,13 +1,25 @@
 package se.ju.student.kade1796.studyassist
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Spinner
 
 
 //This is Hawkars project
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val createButton = findViewById<Button>(R.id.create_button)
+
+        createButton.setOnClickListener{
+            startActivity(
+                    Intent(this, CreateThreadActivity::class.java)
+            )
+        }
     }
 }
--- app/src/main/java/se/ju/student/kade1796/studyassist/ThreadsActivity.kt
+++ app/src/main/java/se/ju/student/kade1796/studyassist/ThreadsActivity.kt
@@ -2,10 +2,13 @@ package se.ju.student.kade1796.studyassist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 
 class ThreadsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_threads)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
     }
 }
