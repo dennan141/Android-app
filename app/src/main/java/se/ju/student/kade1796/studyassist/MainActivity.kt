9bbbaa2d5738fb702275e89158a166ae8bcc6449
Eddiemix33 <62739265+Eddiemix33@users.noreply.github.com>
2021-02-24 21:52:56 +0100

    Skelettet av main activity och navigations baren är klar
    Som titeln säger. Alla knappar ska fungera. Har inte lagt någon funktion på dom ännu dock.
    Måste lägga in färger på navigations-baren och bilder till kategoriknapparna.

--- app/src/main/java/se/ju/student/kade1796/studyassist/MainActivity.kt
+++ app/src/main/java/se/ju/student/kade1796/studyassist/MainActivity.kt
@@ -2,17 +2,74 @@ package se.ju.student.kade1796.studyassist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.ImageButton
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val category1_button = findViewById<ImageButton>(R.id.category1)
+        val categoryCampus          = findViewById<ImageButton>(R.id.category_campus)
+        val categorySocialLife      = findViewById<ImageButton>(R.id.category_social_life)
+        val categoryStudyHelp       = findViewById<ImageButton>(R.id.category_study_help)
+        val categoryMath            = findViewById<ImageButton>(R.id.category_math)
+        val categoryCourseMaterial  = findViewById<ImageButton>(R.id.category_course_material)
+        val categoryITHelp          = findViewById<ImageButton>(R.id.category_it_help)
+        val categoryExchangeStudies = findViewById<ImageButton>(R.id.category_exchange_studies)
+        val categoryOther           = findViewById<ImageButton>(R.id.category_other)
 
-        category1_button.setOnClickListener {
-            //Start next activity
+        categoryCampus.setOnClickListener {
+
+        }
+
+        categorySocialLife.setOnClickListener {
+
+        }
+
+        categoryStudyHelp.setOnClickListener {
+
+        }
+
+        categoryMath.setOnClickListener {
+
+        }
+
+        categoryCourseMaterial.setOnClickListener {
+
+        }
+
+        categoryITHelp.setOnClickListener {
+
+        }
+
+        categoryExchangeStudies.setOnClickListener {
+
+        }
+
+        categoryOther.setOnClickListener {
+
+        }
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.nav_bar)
+
+        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+            when(item.itemId){
+                R.id.page_home -> {
+                    //Return to main activity
+                    true
+                }
+                R.id.page_account -> {
+                    //Open the account activity
+                    true
+                }
+                R.id.page_post -> {
+                    //Open the create view activity
+                    true
+                }
+                else -> false
+            }
         }
     }
 
