9c48b35dff2f86dc1ca60650c7e28375afc3e532
hawkarJamal1997 <jaha1710@student.ju.se>
2021-03-07 20:11:04 +0100

    failed to push all files and changes from last commit

--- app/src/main/java/se/ju/student/kade1796/studyassist/ThreadAdapter.kt
+++ app/src/main/java/se/ju/student/kade1796/studyassist/ThreadAdapter.kt
@@ -1,4 +1,47 @@
 package se.ju.student.kade1796.studyassist
 
-class ThreadAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.thread_item.view.*
+
+class ThreadAdapter(
+    var threadList: List<Thread>,
+    val listener: OnItemClickListener
+) : RecyclerView.Adapter<ThreadAdapter.ThreadViewHolder>() {
+
+    override fun onCreateViewHolder(ThreadsActivty: ViewGroup, viewType: Int): ThreadViewHolder {
+        val itemView = LayoutInflater.from(ThreadsActivty.context).inflate(R.layout.thread_item, ThreadsActivty, false)
+        return ThreadViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: ThreadViewHolder, position: Int) {
+        holder.titleTextView.text = threadList[position].title
+        holder.descriptionTextView.text = threadList[position].content
+    }
+
+    override fun getItemCount() = threadList.size
+
+    inner class ThreadViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView),
+    View.OnClickListener{
+        val titleTextView: TextView = itemView.title_textView
+        val descriptionTextView: TextView = itemView.description_textView
+
+        init {
+            itemView.setOnClickListener(this)
+        }
+
+        override fun onClick(v: View?) {
+            val position = adapterPosition
+            if(position != RecyclerView.NO_POSITION) {
+                listener.onItemClick(position)
+            }
+        }
+    }
+
+    interface OnItemClickListener{
+        fun onItemClick(position: Int)
+    }
 }
--- app/src/main/java/se/ju/student/kade1796/studyassist/ThreadDetailActivity.kt
+++ app/src/main/java/se/ju/student/kade1796/studyassist/ThreadDetailActivity.kt
@@ -1,11 +1,22 @@
 package se.ju.student.kade1796.studyassist
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_thread_detail.*
 import android.os.Bundle
+import android.widget.TextView
 
 class ThreadDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_thread_detail)
+
+        val titleTextView = findViewById<TextView>(R.id.titleText)
+        val contentTextView = findViewById<TextView>(R.id.contentText)
+        val title  = intent.getStringExtra("title").toString()
+        val content  = intent.getStringExtra("content").toString()
+        titleTextView.text = title
+        contentTextView.text = content
+
     }
 }
