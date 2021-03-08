8c9a65ddb40c083345cc09e96e1ace2c273aa960
dennan141 <dennis.karlsson96@outlook.com>
2021-03-08 21:41:33 +0100

    commented error fixes todo

--- app/src/main/java/se/ju/student/kade1796/studyassist/ThreadAdapter.kt
+++ app/src/main/java/se/ju/student/kade1796/studyassist/ThreadAdapter.kt
@@ -26,8 +26,13 @@ class ThreadAdapter(
 
     inner class ThreadViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView),
     View.OnClickListener{
+
+
+        //TODO: FIX ERROR UNRESOLVED REFERENCE !!!
         val titleTextView: TextView = itemView.title_textView
         val descriptionTextView: TextView = itemView.description_textView
+        //TODO: FIX ERROR UNRESOLVED REFERENCE !!!
+
 
         init {
             itemView.setOnClickListener(this)
